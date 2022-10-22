c7096089d9b08ff479bc935f2a606cba8e613af4
Igor Rodrigues <igor220597@hotmail.com>
2022-10-22 17:51:43 -0300

    wip: adiciona fluxo de login

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Login from "./pages/Login";
+import Homepage from "./pages/HomePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+
+            <Route path="/homepage" element={<Homepage />} />
+
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
